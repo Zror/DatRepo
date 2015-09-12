5aad90b1f74e0037948114b67601fc6c2f2c49b1
srwronsk <srwronsk@mtu.edu>
2015-09-12 15:13:46 -0400

    Basic Unity Project

---
+++ Dragon/Assets/Dragon/Scripts/Dragon/Wings.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wings : MonoBehaviour {
+
+    public float velocity;
+
+    private Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(new Vector2(0, velocity));
+        }
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+}
