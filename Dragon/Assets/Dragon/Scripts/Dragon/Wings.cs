374c57cf5f8256fade666e4904f414d0561f3c09
James Lutey <jace.lutey@gmail.com>
2015-09-19 19:12:38 -0400

    Added an updraft method to wings.

--- Dragon/Assets/Dragon/Scripts/Dragon/Wings.cs
+++ Dragon/Assets/Dragon/Scripts/Dragon/Wings.cs
@@ -7,10 +7,24 @@ public class Wings : MonoBehaviour {
 
     private Rigidbody2D rigidbody;
 
+    private bool overFire;
+
+    private float grav;
+
 	// Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody2D>();
 	}
+
+    //Use this for gliding over fires
+    void Updraft() {
+      /*
+      At the moment this is just bound to a key, 
+      this will be changed when we have a fire object and a way to check for collision 
+      between the dragon and the air above a fire.
+      */ 
+    rigidbody.AddForce(new Vector2(0, 1.25F * velocity));
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,7 +32,11 @@ public class Wings : MonoBehaviour {
         {
             rigidbody.AddForce(new Vector2(0, velocity));
         }
-	}
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            Updraft();
+        }
+    }
 
     void FixedUpdate()
     {
