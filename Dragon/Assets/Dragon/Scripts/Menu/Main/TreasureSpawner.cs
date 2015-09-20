a6fc539fee142457178f6216fa84b46fdd74760b
sam wronski <runewake2@outlook.com>
2015-09-19 20:43:15 -0400

    Added a main menu scene. Added a new `TreasureSpawner` class which will handle creating a pile of loot. Began setting up `TreasureSpawner` so it can spawn infinitely varied types of loot.

---
+++ Dragon/Assets/Dragon/Scripts/Menu/Main/TreasureSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreasureSpawner : MonoBehaviour {
+
+    public int treasureAmount;
+    public WeightedTreasure[] treasurePile = new WeightedTreasure[] {
+        new WeightedTreasure() { weight = 10, treasure = null }
+    };
+    public float spawnRate;
+    public float positionJitter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
