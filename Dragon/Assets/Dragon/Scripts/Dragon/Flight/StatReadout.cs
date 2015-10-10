1588ebe6a792d35b63e699e2263611608cfd36f7
Luteyjam <jace.lutey@gmaill.com>
2015-10-10 19:00:57 -0400

    Changed how flight works

---
+++ Dragon/Assets/Dragon/Scripts/Dragon/Flight/StatReadout.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatReadout : MonoBehaviour {
+	public Vector2 velocity;
+
+	private Rigidbody2D body;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		velocity = body.velocity;
+	}
+
+}
--- Dragon/Assets/Dragon/Scripts/Dragon/Flight/Wings.cs
+++ Dragon/Assets/Dragon/Scripts/Dragon/Flight/Wings.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Wings : MonoBehaviour {
-
+public class Wings {
+/*
     public float velocity;
 
     private Rigidbody2D rigidbody;
@@ -12,26 +12,26 @@ public class Wings : MonoBehaviour {
     private float grav;
 
 	// Use this for initialization
-	void Start () {
-        rigidbody = GetComponent<Rigidbody2D>();
-	}
+	//void Start () {
+    
+	//}
 
     //Use this for gliding over fires
     void Updraft() {
-      /*
+      
       At the moment this is just bound to a key, 
       this will be changed when we have a fire object and a way to check for collision 
       between the dragon and the air above a fire.
-      */ 
+      
     rigidbody.AddForce(new Vector2(0, 1.25F * velocity));
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rigidbody.AddForce(new Vector2(velocity * .55F, velocity));
-        }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+            //rigidbody.AddForce(new Vector2(velocity * .55F, velocity));
+        //}
         if (Input.GetKeyDown(KeyCode.LeftAlt))
         {
             Updraft();
@@ -42,4 +42,5 @@ public class Wings : MonoBehaviour {
     {
 
     }
+	*/ 
 }
