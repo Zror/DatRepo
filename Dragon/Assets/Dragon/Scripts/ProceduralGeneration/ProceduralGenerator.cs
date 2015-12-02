70e174f57a8c488806adb881f0456b3a1cc28c69
sam wronski <srwronsk@mtu.edu>
2015-12-01 22:01:50 -0500

    Procedural World Terrain Generation.

--- Dragon/Assets/Dragon/Scripts/ProceduralGeneration/ProceduralGenerator.cs
+++ Dragon/Assets/Dragon/Scripts/ProceduralGeneration/ProceduralGenerator.cs
@@ -1,62 +1,182 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 
+[RequireComponent(typeof(ProceduralWorldSettings))]
 public class ProceduralGenerator : MonoBehaviour
 {
-    public GameObject root;
-    private ProceduralObject lastSpawned;
+    [Header("Game Object Pool")]
+    public GameObject grass;
+    public GameObject swamp;
+    public GameObject dirt;
+    public GameObject[] towers;
+    public GameObject[] clouds;
+    public GameObject[] storms;
+    public GameObject[] coins;
+    public GameObject[] haystacks;
+    public GameObject[] livestocks;
 
-    public float spawnX;
-    public float endSpawn;
+    [Header("Generator Configuration")]
+    [Tooltip("This origin will be reset at the start of the scene.")]
+    public FloatingOrigin origin;
+
+    public float border = 10;
+    public float stepSize = 2;
+    public float offset = 0;
+
+    protected ProceduralWorldSettings settings;
+
+    [Header("Spawn Counters (Debugging Display)")]
+    public float towerSpawnCounter;
+    public float cloudSpawnCounter;
+    public float stormCloudSpawnCounter;
+    public float coinSpawnCounter;
+    public float haystackSpawnCounter;
+    public float livestockSpawnCounter;
 
     protected void Start()
     {
-        GenerateAll();
-        SpawnObject(root);
+        origin = FindObjectOfType<FloatingOrigin>();
+        settings = GetComponent<ProceduralWorldSettings>();
+        origin.OnOriginShift += PostShiftGeneration;
+
+        this.transform.position = Vector3.left*origin.clearChildAfter;
+        GenerateNewNodes();
     }
 
-    private void GenerateAll()
+    protected void PostShiftGeneration(object sender, FloatingOriginShiftEventArgs args)
     {
-        SpawnObject(root);
-        var initialSpawn = lastSpawned;
+        this.transform.position += Vector3.left*args.ShiftAmount;
+        GenerateNewNodes();
+    }
 
-        LinkedList<ProceduralObject> spawned = new LinkedList<ProceduralObject>();
-        spawned.AddFirst(initialSpawn);
-
-        while (initialSpawn.transform.position.x > endSpawn)
+    protected void GenerateNewNodes()
+    {
+        while (this.transform.position.x < origin.transform.position.x + origin.maxOriginPoint + border)
         {
-            foreach (var obj in spawned)
-            {
-                obj.transform.position = new Vector3(obj.transform.position.x - (this.transform.position.x  - spawnX), this.transform.position.y, this.transform.position.z);
-            }
-            GenerateNext();
-            spawned.AddLast(lastSpawned);
+            CreateTerrainAtPosition();
+            IncrementCounters();
+
+            offset += stepSize;
+            this.transform.position += Vector3.right*stepSize;
         }
     }
 
-    protected void Update()
+    protected void CreateTerrainAtPosition()
     {
-        if (lastSpawned.transform.position.x < spawnX)
+        float grassValue = settings.GetValueFromCurveAtPosition(settings.grassRate, offset);
+        float swampValue = settings.GetValueFromCurveAtPosition(settings.swampRate, offset);
+        float dirtValue = settings.GetValueFromCurveAtPosition(settings.dirtRate, offset);
+
+        GameObject inst = swamp;
+        if (grassValue > swampValue && grassValue > dirtValue)
         {
-            GenerateNext();
+            inst = grass;
+        }
+        else if (dirtValue > swampValue && dirtValue > grassValue)
+        {
+            inst = dirt;
+        }
+
+        GameObject created = (GameObject)Instantiate(inst, this.transform.position, this.transform.rotation);
+        var sprites = created.GetComponents<SpriteRenderer>();
+        foreach (var sprite in sprites)
+        {
+            sprite.sortingOrder = (int) offset;
+        }
+        created.transform.parent = origin.transform;
+    }
+
+    protected void IncrementCounters()
+    {
+        towerSpawnCounter += settings.GetValueFromCurveAtPosition(settings.towerRate, offset)*stepSize;
+        cloudSpawnCounter += settings.GetValueFromCurveAtPosition(settings.cloudRate, offset)*stepSize;
+        stormCloudSpawnCounter += settings.GetValueFromCurveAtPosition(settings.stormCloudRate, offset)*stepSize;
+        coinSpawnCounter += settings.GetValueFromCurveAtPosition(settings.coinRate, offset)*stepSize;
+        haystackSpawnCounter += settings.GetValueFromCurveAtPosition(settings.haystackRate, offset)*stepSize;
+        livestockSpawnCounter += settings.GetValueFromCurveAtPosition(settings.livestockRate, offset)*stepSize;
+
+        if (towerSpawnCounter > settings.towerSpawnAt)
+        {
+            towerSpawnCounter -= settings.towerSpawnAt;
+            CreateTower();
+        }
+
+        if (cloudSpawnCounter > settings.cloudSpawnAt)
+        {
+            cloudSpawnCounter -= settings.cloudSpawnAt;
+            CreateCloud();
+        }
+
+        if (stormCloudSpawnCounter > settings.stormCloudSpawnAt)
+        {
+            stormCloudSpawnCounter -= settings.stormCloudSpawnAt;
+            CreateStormCloud();
+        }
+
+        if (coinSpawnCounter > settings.coinSpawnAt)
+        {
+            coinSpawnCounter -= settings.coinSpawnAt;
+            CreateCoin();
+        }
+
+        if (haystackSpawnCounter > settings.haystackSpawnAt)
+        {
+            haystackSpawnCounter -= settings.haystackSpawnAt;
+            CreateHaystack();
+        }
+
+        if (livestockSpawnCounter > settings.livestockSpawnAt)
+        {
+            livestockSpawnCounter -= settings.livestockSpawnAt;
+            CreateLivestock();
         }
     }
 
-    public void GenerateNext()
+    protected void CreateTower()
     {
-        SpawnObject(SelectNextSpawn());
+        CreateObject(SelectOne(towers));
     }
 
-    private void SpawnObject(GameObject instance)
+    protected void CreateCloud()
     {
-        GameObject spawned = (GameObject) Instantiate(instance, this.transform.position, this.transform.rotation);
-        lastSpawned = spawned.GetComponent<ProceduralObject>();
 
+        CreateObject(SelectOne(clouds));
     }
 
-    private GameObject SelectNextSpawn()
-    {;
-        return lastSpawned.potentialNextNodes[Random.Range(0, lastSpawned.potentialNextNodes.Length)];
+    protected void CreateStormCloud()
+    {
+
+        CreateObject(SelectOne(storms));
+    }
+
+    protected void CreateCoin()
+    {
+
+        CreateObject(SelectOne(coins));
+    }
+
+    protected void CreateHaystack()
+    {
+
+        CreateObject(SelectOne(haystacks));
+    }
+
+    protected void CreateLivestock()
+    {
+        CreateObject(SelectOne(livestocks));
+    }
+
+    protected void CreateObject(GameObject template)
+    {
+        GameObject created = (GameObject)Instantiate(template, this.transform.position, this.transform.rotation);
+        created.transform.parent = origin.transform; // Make a parent of the floating origin
+    }
+
+    protected TObject SelectOne<TObject>(TObject[] objects)
+    {
+        return objects[UnityEngine.Random.Range(0, objects.Length)];
     }
 }
--- Dragon/Assets/Dragon/Scripts/ProceduralGeneration/ProceduralObject.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ProceduralObject : MonoBehaviour
-{
-    public GameObject[] potentialNextNodes;
-}
