b5659bc452fe3e5bdf49cf78d34f45ef593a6944
BrettKriz <bakriz@mtu.edu>
2016-01-30 16:41:36 -0500

    Fixed the princess prefab in resources!

---
+++ Dragon/Assets/Dragon/Scripts/Items/Princess_Script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Princess_Script : MonoBehaviour {
+
+    public Session_Monitor monitor;
+
+    // Use this for initialization
+    void Start () {
+        this.monitor = FindObjectOfType<Session_Monitor>();
+    }
+	    
+    void FixedUpdate()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // Reward a princess and remove
+        monitor.Princesses_Taken();
+
+        Destroy(this.gameObject);
+    }
+
+}
