bd7d7c8eae24d3267ccd3d3e2ca95e46e2bc946d
James Lutey <jace.lutey@gmail.com>
2016-03-19 15:49:41 -0400

    Fixing my code that was reverted.

--- Dragon/Assets/Dragon/Scripts/Dragon/Flight/HangGlider.cs
+++ Dragon/Assets/Dragon/Scripts/Dragon/Flight/HangGlider.cs
@@ -5,10 +5,12 @@ public class HangGlider : Flight {
 
 	// Update is called once per frame
 	public override void Update () {
-		if (Input.GetKeyDown(KeyCode.Space)){
-			if( rigidbody.velocity.x > 15){
+        Input.simulateMouseWithTouches = true;
+        if (Input.GetMouseButtonDown(0) && health.HasStamina && clickLeft()) { 
+            if ( rigidbody.velocity.x > 15){
 				rigidbody.AddForce(new Vector2(velocity * .55F, velocity));
-			}
+                health.ChangeStamina(-10);
+            }
 			else
 				rigidbody.AddForce(new Vector2(velocity * .55F, 0));
 		}
