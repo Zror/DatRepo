0bbace5aacc487b813a40a705afd6b9bba42d9f9
Daniel <djsaltzm@mtu.edu>
2016-03-26 13:26:28 -0400

    adding knights

---
+++ Dragon/Assets/Dragon/Prefabs/NPCs/knight/night.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class night : MonoBehaviour {
+    public bool Walk;
+    private Animator ani;
+    // Use this for initialization
+    void Start () {
+        ani = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ani.SetBool("Walk", Walk);
+    }
+}
