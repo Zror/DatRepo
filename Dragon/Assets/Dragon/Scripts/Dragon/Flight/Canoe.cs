9ef4a66a0e39b709ab8fd107870de4774cc31f99
Daniel <djsaltzm@mtu.edu>
2016-04-16 19:26:53 -0400

    things

--- Dragon/Assets/Dragon/Scripts/Dragon/Flight/Canoe.cs
+++ Dragon/Assets/Dragon/Scripts/Dragon/Flight/Canoe.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class Canoe : Flight {
-
+    float freq;
     public override void Start()
     {
+        freq = 5.0f;
         base.Start();
-        rigidbody.gravityScale = 0;
+        rigidbody.gravityScale = 0.1f;
     }
     // Update is called once per frame
-    public override void Update () { 
-       
+    public override void Update () {
+        if (freq <= 0)
+        {
+            Random.seed = System.DateTime.Now.Millisecond;
+            rigidbody.gravityScale = (Random.Range(0, 100)) / 100.0f;
+            freq += 5;
+        }
+        freq -= Time.deltaTime;
+        base.Update();
     }
 }
